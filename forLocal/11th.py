b4d477ad29790387767a7de02d58ad2e808b94f9
mirinrimi <61516874+mirinrimi@users.noreply.github.com>
2021-02-27 15:49:46 +0900

    testができた！

--- forLocal/11th.py
+++ forLocal/11th.py
@@ -1,7 +1,9 @@
 from graphillion import GraphSet
 import networkx as nx
+import numpy as np
 from tree_univ import MakeUniverse, DrawGraph, OperationtoTrees
 from ChangeCoord import ChangeCoordandSerial
+import csv
 
 def writelist(list, path):
     strize = [str(x) for x in list]
@@ -9,20 +11,25 @@ def writelist(list, path):
     with open(path, mode='w') as f:
         f.write(str(linebreak))
 
+def writecsv(array, path): # array は二次元配列
+    with open(path, "w") as f:
+        writer = csv.writer(f, delimiter=",")
+        writer.writerows(array)
 
 
 n = 8
 [wtd_tree, wtd] = MakeUniverse().tu_wtd(n)
 GraphSet.set_universe(wtd_tree)
 edges = list(wtd_tree)
-listoflist = []
+arrayedge = np.array(edges)
 
-for e in edges:
-    E = [e]
-    listoflist.append(E)
+a = arrayedge
+print(a)
+print(len(a))
+np.savetxt("tests/test.txt", a, fmt="%d")
 
-tree = GraphSet.trees(root=0, is_spanning=False)
-gs = tree.len(n-1)
-tr = list(gs)
+# with open("tests/test.txt") as f:
+#    print(f.read())
 
-writelist(tr, "edges.txt")
+# print(np.loadtxt("tests/test.txt"))
+print(len(np.loadtxt("tests/test.txt")))
--- tests/inverse_test.py
+++ tests/inverse_test.py
@@ -1,38 +1,43 @@
 import unittest
 from pathlib import Path
+import numpy as np
 import sys
 sys.path.append(str(Path(__file__).parent.parent))
 sys.path.append(str(Path(__file__).parent.parent / "forLocal"))
 
-import forLocal.ChangeCoord.ChangeCoordandSerial
-# from forLocal.ChangeCoord import Changehogehoge
-
+import forLocal.ChangeCoord
 from graphillion import GraphSet
 
 
-'''class TestChanging(unittest.TestCase):
+class TestChanging(unittest.TestCase):
     def setUp(self):
         
-        self.c = ChangeCoord.ChangeCoordandSerial()       
+        self.c = forLocal.ChangeCoord.ChangeCoordandSerial()       
 
-    def test_onecase(self):
+    def test_onecase_inSerial(self):
         self.g = [(0, 1), (0, 2), (0, 3), (1, 8), (1, 9), (8, 15), (9, 16)] 
         self.cg = self.c.SerialtoCoord(self.g)
         self.scg = self.c.CoordtoSerial(self.cg)
         self.assertEqual(self.g, list(self.scg))
 
     def test_inSerial(self):
-        with open("edges.txt", mode="r") as self.f:
-            for self.line in self.f:
-                self.g = self.line.replace("\n", "")
-                self.cg = self.c.SerialtoCoord(self.g, 8)
-                self.scg = self.c.CoordtoSerial(self.cg, 8)
-                self.assertEqual(self.g, list(self.scg))
-'''
+        self.a = np.loadtxt("tests/test.txt", dtype="int64") 
+        self.array = self.a.tolist()
+        for i in range(len(self.array)):
+            self.t = tuple(self.array[i])
+            self.g = [self.t]
+            self.cg = self.c.SerialtoCoord(self.g, 8)
+            self.scg = self.c.CoordtoSerial(self.cg, 8)
+            self.assertEqual(self.g, list(self.scg))
+    
+    def test_onecase_inCoord(self):
+        self.g = [((-1, 0), (0, 0)), ((-1, 0), (0, 1)), ((-1, 0), (0, 2)), ((0, 0), (1, 0)), ((0, 0), (1, 1)), ((1, 0), (2, 0)), ((1, 1), (2, 1))]
+        self.sg = self.c.CoordtoSerial(self.g)
+        self.csg = self.c.SerialtoCoord(self.sg)
+        self.assertEqual(self.g, list(self.csg)) 
 
+# 一般の座標表示g' と csg' の比較も書かなきゃいけない気がするけど、めんどいのでやりません。
 
-# if __name__ == "__main__":
-#    unittest.main()
+if __name__ == "__main__":
+    unittest.main()
 
-print(Path(__file__))
-# print(sys.path)
